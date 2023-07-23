4f74f26edac1f9a8e688034e19e8051f16903501
mina <sadeghbayan.mina@gmail.com>
2023-07-23 14:10:01 +0330

    main slider added

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 const swiper = new Swiper(".swiper", {
-    // Optional parameters
-    speed: 500,
-  
-    // If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  // Optional parameters
+  speed: 500,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var Myswiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
